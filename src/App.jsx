199d84a8d81be2bb06649765e3c7d6fa76a86123
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-08 13:05:25 +0530

    vercel deploy commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,17 @@
 
 // export default App;
 
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
